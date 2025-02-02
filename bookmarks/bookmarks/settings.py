cc2d98063d91d50e87e23abd9ffcd635788c9786
Shekhar Ghimire <shekhar.ghimire1001@gmail.com>
2025-02-02 20:09:11 +0545

    Small change in the key access

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6ezise$ma-$klm_exx7$z9%vsbo!%mcxq)^4#9fw(54)nr(119'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
